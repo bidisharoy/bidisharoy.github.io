f5e0bc71cfbfbe320145dbeb41ef0234ddc4f8f9
Bidisha Roy <bidishaxroy@gmail.com>
2019-10-20 16:45:09 -0400

    new JS file

---
+++ homework6/cart.js
@@ -0,0 +1,17 @@
+/*var addToCartButton = document.getelementbyclassname('addToCart')
+for (var i = 0; i < addToCartButton.length, i++) {
+	var button = addToCart[i]
+	button.addEventListener('click', function(event) {
+		var buttonClicked = event.target
+		buttonClicked.
+	})
+}
+*/
+var pickColorButton = document.getelementbyclassname('box')
+for (var i = 0; i < box.length, i++) {
+	var color = box[i]
+	button.addEventListener('click', function(event) {
+		var buttonClicked = event.target
+		buttonClicked.
+	})
+}
