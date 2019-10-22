039d78f096f13044104bc6176e45390c5dbf4465
Bidisha Roy <bidishaxroy@gmail.com>
2019-10-22 14:05:58 -0400

    more JS
    new JS allows selection of color + fill

--- assgn6/cart.js
+++ assgn6/cart.js
@@ -1,4 +1,148 @@
-/*var addToCartButton = document.getelementbyclassname('addToCart')
+//ADD TO CART BUTTON + CART ICON CLICK COUNTER 
+//timer credit to https://stackoverflow.com/questions/38393740/change-content-of-html-element-div-for-few-seconds
+var clicks = 0;
+var originalText = document.getElementById('cartButton').innerHTML;
+document.getElementById('cartButton').onclick=addedToCart;
+function addedToCart(){
+	document.getElementById('cartButton').innerHTML="<span style='color:#E4C442;'>Added!</span";
+	clicks +=1;
+	document.getElementById("clicks").innerHTML=clicks;
+	setTimeout(function() {
+		document.getElementById('cartButton').innerHTML=originalText;
+	}, 950);
+};
+
+//PICKING COLORS
+var originalStyleASS = document.getElementById('pickColorASS').innerHTML;
+var originalStyleMH = document.getElementById('pickColorMH').innerHTML;
+var originalStyleRD = document.getElementById('pickColorRD').innerHTML;
+var originalStyleCD = document.getElementById('pickColorCD').innerHTML;
+
+document.getElementById('pickColorASS').onclick=pickedColorASS;
+function pickedColorASS(){
+	document.getElementById('pickColorASS').style.border='outset black';
+	document.getElementById('pickColorASS').innerHTML=originalStyleASS;
+	document.getElementById('pickColorMH').style.border='solid 1px';
+	document.getElementById('pickColorMH').innerHTML=originalStyleMH;
+	document.getElementById('pickColorMH').style.color='gray';
+	document.getElementById('pickColorMHText').style.color='gray';
+	document.getElementById('pickColorRD').style.border='solid 1px';
+	document.getElementById('pickColorRD').innerHTML=originalStyleRD;
+	document.getElementById('pickColorRD').style.color='gray';
+	document.getElementById('pickColorRDText').style.color='gray';
+	document.getElementById('pickColorCD').style.border='solid 1px';
+	document.getElementById('pickColorCD').innerHTML=originalStyleCD;
+	document.getElementById('pickColorCD').style.color='gray';
+	document.getElementById('pickColorCDText').style.color='gray';
+};
+
+document.getElementById('pickColorMH').onclick=pickedColorMH;
+function pickedColorMH(){
+	document.getElementById('pickColorMH').style.border='outset black';
+	document.getElementById('pickColorMH').innerHTML=originalStyleMH;
+	document.getElementById('pickColorASS').style.border='solid 1px';
+	document.getElementById('pickColorASS').innerHTML=originalStyleASS;
+	document.getElementById('pickColorASS').style.color='gray';
+	document.getElementById('pickColorASSText').style.color='gray';
+	document.getElementById('pickColorRD').style.border='solid 1px';
+	document.getElementById('pickColorRD').innerHTML=originalStyleRD;
+	document.getElementById('pickColorRD').style.color='gray';
+	document.getElementById('pickColorRDText').style.color='gray';
+	document.getElementById('pickColorCD').style.border='solid 1px';
+	document.getElementById('pickColorCD').innerHTML=originalStyleCD;
+	document.getElementById('pickColorCD').style.color='gray';
+	document.getElementById('pickColorCDText').style.color='gray';
+};
+
+document.getElementById('pickColorRD').onclick=pickedColorRD;
+function pickedColorRD(){
+	document.getElementById('pickColorRD').style.border='outset black';
+	document.getElementById('pickColorRD').innerHTML=originalStyleRD;
+	document.getElementById('pickColorASS').style.border='solid 1px';
+	document.getElementById('pickColorASS').innerHTML=originalStyleASS;
+	document.getElementById('pickColorASS').style.color='gray';
+	document.getElementById('pickColorASSText').style.color='gray';
+	document.getElementById('pickColorMH').style.border='solid 1px';
+	document.getElementById('pickColorMH').innerHTML=originalStyleMH;
+	document.getElementById('pickColorMH').style.color='gray';
+	document.getElementById('pickColorMHText').style.color='gray';
+	document.getElementById('pickColorCD').style.border='solid 1px';
+	document.getElementById('pickColorCD').innerHTML=originalStyleCD;
+	document.getElementById('pickColorCD').style.color='gray';
+	document.getElementById('pickColorCDText').style.color='gray';
+};
+
+document.getElementById('pickColorCD').onclick=pickedColorCD;
+function pickedColorCD(){
+	document.getElementById('pickColorCD').style.border='outset black';
+	document.getElementById('pickColorCD').innerHTML=originalStyleCD;
+	document.getElementById('pickColorASS').style.border='solid 1px';
+	document.getElementById('pickColorASS').innerHTML=originalStyleASS;
+	document.getElementById('pickColorASS').style.color='gray';
+	document.getElementById('pickColorASSText').style.color='gray';
+	document.getElementById('pickColorMH').style.border='solid 1px';
+	document.getElementById('pickColorMH').innerHTML=originalStyleMH;
+	document.getElementById('pickColorMH').style.color='gray';
+	document.getElementById('pickColorMHText').style.color='gray';
+	document.getElementById('pickColorRD').style.border='solid 1px';
+	document.getElementById('pickColorRD').innerHTML=originalStyleRD;
+	document.getElementById('pickColorRD').style.color='gray';
+	document.getElementById('pickColorRDText').style.color='gray';
+};
+
+//PICKING FILLINGS
+var originalStyleDD = document.getElementById('pickFillDD').innerHTML;
+var originalStyleHAPB = document.getElementById('pickFillHAPB').innerHTML;
+var originalStyleMD = document.getElementById('pickFillMD').innerHTML;
+
+document.getElementById('pickFillDD').onclick=pickedFillDD;
+function pickedFillDD(){
+	document.getElementById('pickFillDD').style.border='outset black';
+	document.getElementById('pickFillDD').innerHTML=originalStyleDD;
+	document.getElementById('pickFillHAPB').style.border='solid 1px';
+	document.getElementById('pickFillHAPB').innerHTML=originalStyleHAPB;
+	document.getElementById('pickFillHAPB').style.color='gray';
+	document.getElementById('pickFillHAPBText').style.color='gray';
+	document.getElementById('pickFillMD').style.border='solid 1px';
+	document.getElementById('pickFillMD').innerHTML=originalStyleMD;
+	document.getElementById('pickFillMD').style.color='gray';
+	document.getElementById('pickFillMDText').style.color='gray';
+};
+
+document.getElementById('pickFillHAPB').onclick=pickedFillHAPB;
+function pickedFillHAPB(){
+	document.getElementById('pickFillHAPB').style.border='outset black';
+	document.getElementById('pickFillHAPB').innerHTML=originalStyleHAPB;
+	document.getElementById('pickFillDD').style.border='solid 1px';
+	document.getElementById('pickFillDD').innerHTML=originalStyleDD;
+	document.getElementById('pickFillDD').style.color='gray';
+	document.getElementById('pickFillDDText').style.color='gray';
+	document.getElementById('pickFillMD').style.border='solid 1px';
+	document.getElementById('pickFillMD').innerHTML=originalStyleMD;
+	document.getElementById('pickFillMD').style.color='gray';
+	document.getElementById('pickFillMDText').style.color='gray';
+};
+
+document.getElementById('pickFillMD').onclick=pickedFillMD;
+function pickedFillMD(){
+	document.getElementById('pickFillMD').style.border='outset black';
+	document.getElementById('pickFillMD').innerHTML=originalStyleMD;
+	document.getElementById('pickFillDD').style.border='solid 1px';
+	document.getElementById('pickFillDD').innerHTML=originalStyleDD;
+	document.getElementById('pickFillDD').style.color='gray';
+	document.getElementById('pickFillDDText').style.color='gray';
+	document.getElementById('pickFillHAPB').style.border='solid 1px';
+	document.getElementById('pickFillHAPB').innerHTML=originalStyleHAPB;
+	document.getElementById('pickFillHAPB').style.color='gray';
+	document.getElementById('pickFillHAPBText').style.color='gray';
+};
+
+
+/* CODE THAT DID NOT WORK / SAVING FOR LATER
+
+var loserColors=[document.getElementById('pickColorMH'), document.getElementById('pickColorRD'), document.getElementById('pickColorCD')];
+
+var addToCartButton = document.getelementbyclassname('addToCart')
 for (var i = 0; i < addToCartButton.length, i++) {
 	var button = addToCart[i]
 	button.addEventListener('click', function(event) {
@@ -15,62 +159,29 @@ for (var i = 0; i < box.length, i++) {
 		colorSelect.toggleClass('.boxMyClickState')
 	})
 }
-
-
 (function (){
 	let CartInfo = getelementbyid('headerCart')
 })();
-
 
 // x.innerhtml
 // html event attributes (onclick, onmousedown, etc.)
 // local storage vs session storage
 
-
 var addToCartButton = document.getelementbyclassname('Button1Text');
 var headerCart = document.getElementById(headerCart);
-
 addToCartButton.onclick = function()
 {updateHeaderCart()};
-
 function updateHeaderCart() {
 headerCart.innerHTML = "ADDED"
 };
 
-*/
-
-
-//CREDIT TO https://stackoverflow.com/questions/38393740/change-content-of-html-element-div-for-few-seconds
-var clicks = 0;
-var originalText = document.getElementById('cartButton').innerHTML;
-document.getElementById('cartButton').onclick=addedToCart;
-function addedToCart(){
-	//var originalText = document.getElementById('cartButton').innerHTML;
-	document.getElementById('cartButton').innerHTML="<span style='color:#E4C442;'>Added!</span";
-	//document.getElementById('cartButton').innerHTML="<span style='color: red;'>**Message</span>";
-	//cartButton.style.fontSize="30px";
-	clicks +=1;
-	document.getElementById("clicks").innerHTML=clicks;
-	setTimeout(function() {
-		document.getElementById('cartButton').innerHTML=originalText;
-		//originalText.style.fontSize="22px";
-	}, 950);
-	//var clicks = 0;
-	//document.getElementById('cartButton').onclick=clickCounter;
-	//function clickCounter(){
-		//clicks +=1;
-	//document.getElementById("clicks").innerHTML=clicks;
-};
-
-
-/* document.getElementById('cartButton').onclick=countClicks;
+document.getElementById('cartButton').onclick=countClicks;
 var cnt=0;
 function countClicks(){
 	cnt=parseInt(cnt)+parseInt(1);
 	var divData=document.getElementById("cartButton");
 	divData.innerHTML=cnt;
 }
-
 
 var clicks = 0;
 document.getElementById('cartButton').onclick=clickCounter;
